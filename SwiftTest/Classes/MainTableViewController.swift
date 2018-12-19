8452353d109e87f9dcbf7182b733a10f91714c2e
Olexii Strilets <olexiistrilets@gmail.com>
2018-12-19 10:33:01 +0200

    ST-1: Added project file

---
+++ SwiftTest/Classes/MainTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainTableViewController.swift
+//  SwiftTest
+//
+//  Created by Olexii Strilets on 12/19/18.
+//  Copyright © 2018 supersonic. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+}
